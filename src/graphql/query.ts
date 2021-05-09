759d3d52d816ebe5e5fa645623f2bdaba9d30118
vitoladev <asaptorvi@protonmail.com>
2021-05-09 02:00:22 -0300

    feat(api): get all users query

---
+++ src/graphql/query.ts
@@ -0,0 +1,15 @@
+import { objectType } from 'nexus';
+
+const Query = objectType({
+  name: 'Query',
+  definition(t) {
+    t.nonNull.list.nonNull.field('allUsers', {
+      type: 'User',
+      resolve: (_parent, _args, context) => {
+        return context.prisma.user.findMany();
+      }
+    });
+  }
+});
+
+export default Query;
