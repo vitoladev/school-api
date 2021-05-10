579d65be5c8c7828792d825e1d11f0f71b16c0a1
vitoladev <asaptorvi@protonmail.com>
2021-05-10 03:34:21 -0300

    feat(mutation): update user

--- src/resolvers/User/mutation.ts
+++ src/resolvers/User/mutation.ts
@@ -1,21 +1,38 @@
-import { extendType, inputObjectType, intArg, nonNull, stringArg } from 'nexus';
+import { arg, extendType, intArg, nonNull, stringArg } from 'nexus';
 
 export const UserMutation = extendType({
   type: 'Mutation',
   definition(t) {
     t.nonNull.field('createUser', {
       type: 'User',
+      description: 'creates a new user',
       args: {
-        name: nonNull(stringArg()),
-        email: nonNull(stringArg()),
-        roleId: nonNull(intArg())
+        createUserInput: nonNull(arg({ type: 'CreateUserInput' }))
       },
-      async resolve(_, { name, email, roleId }, { prisma }) {
+      async resolve(_, { createUserInput }, { prisma }) {
         return await prisma.user.create({
           data: {
-            name: name,
-            email: email,
-            roleId: roleId
+            name: createUserInput.name,
+            email: createUserInput.email,
+            roleId: createUserInput.roleId
+          }
+        });
+      }
+    });
+
+    t.nonNull.field('updateUser', {
+      type: 'User',
+      description: 'updates a user',
+      args: {
+        updateUserInput: nonNull(arg({ type: 'UpdateUserInput' }))
+      },
+      async resolve(_, { updateUserInput }, { prisma }) {
+        return await prisma.user.update({
+          where: { id: updateUserInput.id },
+          data: {
+            name: updateUserInput.name,
+            email: updateUserInput.email,
+            roleId: updateUserInput.roleId
           }
         });
       }
