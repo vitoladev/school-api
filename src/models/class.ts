fc15b36dfc71443c0d42c254780f06c976712016
vitoladev <asaptorvi@protonmail.com>
2021-05-12 23:29:03 -0300

    feat(models): class

---
+++ src/models/class.ts
@@ -0,0 +1,19 @@
+import { objectType } from 'nexus';
+
+export const Class = objectType({
+  name: 'Class',
+  definition(t) {
+    t.nonNull.int('id');
+    t.nonNull.string('description');
+    t.list.field('registrations', {
+      type: 'Registration',
+      resolve: async (parent, __, { prisma }) => {
+        return await prisma.class.findUnique({
+          where: { id: parent.id || undefined }
+        })
+        .registrations();
+      }
+    });
+    t.nonNull.field('createdAt', { type: 'DateTime' });
+  }
+});
